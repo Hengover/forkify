edf3f83d31d4e356c7b672f5099091a39b5a6817
Hengover <60360402+Hengover@users.noreply.github.com>
2020-06-21 14:51:26 +0300

    Building the Recipe model Part 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 // Global app controller
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchViews';
 import { elements, renderLoader, clearLoader } from './views/base';
 
@@ -11,6 +12,7 @@ import { elements, renderLoader, clearLoader } from './views/base';
  */
 const state = {};
 
+/**Search conroller */
 const controlSearch = async () => {
     // 1. Get query from view
     const query = searchView.getInput(); //Get input from form
@@ -23,13 +25,18 @@ const controlSearch = async () => {
         searchView.clearInput();
         searchView.clearResults();
         renderLoader(elements.searchRes);
+        try {
+            // 4. Search for recipes
+            await state.search.getResults();
 
-        // 4. Search for recipes
-        await state.search.getResults();
-
-        // 5. Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+            // 5. Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch (err) {
+            alert('Something wrong with the search!')
+            clearLoader();
+        }
+        
     }
 }
 
@@ -39,10 +46,52 @@ elements.searchForm.addEventListener('submit', e => {
 });
 
 elements.searchResPages.addEventListener('click', e => {
-    const btn = e.target.closest('.btn-inline'); //Return closest element   
+    const btn = e.target.closest('.btn-inline'); //Return closest element  
+     
     if (btn) {
         const goToPage = parseInt(btn.dataset.goto, 10); //Return data atributes string
         searchView.clearResults();
         searchView.renderResults(state.search.result, goToPage)
     }
 })
+
+
+/**
+ * Recipe controller
+ */
+
+const controlRecipe = async () => {
+    //Get ID from url
+    const id = window.location.hash.replace('#', ''); //Return entire url( in this case - hash symbole - id)
+    console.log(id);
+
+    if(id) {
+        //Prepare UI for changes
+
+        //Create new recipe object
+        state.recipe = new Recipe(id);
+
+        try {
+           //Get recipe data
+            await state.recipe.getRecipe();
+
+            //Calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+            
+            //Render recipe
+            console.log(state.recipe);
+
+        } catch (err) {
+            alert('Error processing recipe!')
+        }
+        
+    }
+};
+
+/*
+window.addEventListener('hashchange', controlRecipe); //When we change hash this return controlRecipe function
+window.addEventListener('load', controlRecipe); //It fired whenever page located (For example - when user load page from sidebar)
+*/
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
